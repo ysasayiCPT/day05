bb1dd4d6d09efa49782de31cb1c5248e61091fe4
ysasayiCPT <67149065+ysasayiCPT@users.noreply.github.com>
2020-06-24 09:19:09 +0200

    Update ft_strncpy.c

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -11,15 +11,6 @@
 /* ************************************************************************** */
 #include<stdio.h>
 char *ft_strncpy(char *dest, char *src, unsigned int n );
-int main()
-{
-	char str1[]= "myString";
-	int n = sizeof(str1)/ sizeof(str1[0]);
-	char str2[n];
-	ft_strncpy(str2, str1, 6);
-    printf("%s", str2);
-	return 0;
-}
 
 char *ft_strncpy(char *dest, char *src, unsigned int n)
 {

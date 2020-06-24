9583effc676dbde28cb5691c92273cf340e68547
ysasayiCPT <67149065+ysasayiCPT@users.noreply.github.com>
2020-06-24 09:20:28 +0200

    Update ft_strstr.c

--- ex02/ft_strstr.c
+++ ex02/ft_strstr.c
@@ -13,15 +13,6 @@
 #include<stdio.h>
 char *ft_strstr(char *str, char *to_find);
 
-int main()
-{
-	char str1[]= "program";
-	char str2[] ="ram";
-	printf("Returned string is : %s", ft_strstr(str1, str2));
-	return 0;
-
-}
-
 char *ft_strstr(char *str, char *to_find)
 {
 	while(*str)
